53cfd41d2716f49348e60d0a0434ba436f0101e0
Stian Lode <stian.lode@gmail.com>
2012-10-27 00:48:27 +0200

    Added solarized terminal. Dircolors

--- vim/install.sh
+++ vim/install.sh
@@ -15,11 +15,17 @@ https://github.com/skwp/YankRing.vim.git
 https://github.com/tpope/vim-git.git
 https://github.com/gregsexton/gitv.git
 https://github.com/tpope/vim-fugitive.git
+https://github.com/pangloss/vim-javascript.git
+https://github.com/ervandew/supertab.git
 https://github.com/tomtom/tlib_vim.git
 https://github.com/MarcWeber/vim-addon-mw-utils.git
 https://github.com/garbas/vim-snipmate.git
 https://github.com/honza/snipmate-snippets.git
 https://github.com/skwp/vim-powerline.git
+https://github.com/Rip-Rip/clang_complete.git
+https://github.com/scrooloose/nerdtree.git
+https://github.com/jistr/vim-nerdtree-tabs.git
+https://github.com/skwp/vim-colors-solarized.git
 http://github.com/sjl/gundo.vim.git"
 
 if [ -d ~/.vim/bundle ]; then
---
+++ vim/vim.symlink/plugin/settings/clang_complete.vim
@@ -0,0 +1,7 @@
+"========================================
+"	  clang_complete settings
+"========================================
+
+let g:clang_snippets = 1
+"let g:clang_snippets_engine = 'snipmate'
+let g:clang_snippets_engine = 'clang_complete'
