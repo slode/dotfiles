ff12c2d782ce20da8e950fbc79803e832c26bf6a
Stian Lode <sl@rds.no>
2018-05-29 08:20:13 +0200

    Adds lineformatting plugin to zsh. Adds dir history to zsh. Fixes backup script

---
+++ zsh/zsh.symlink/dir_history.zsh
@@ -0,0 +1,3 @@
+DIRSTACKSIZE=8
+setopt autopushd pushdminus pushdsilent pushdtohome pushd_ignore_dups
+alias dh='dirs -v'
---
+++ zsh/zsh.symlink/fmt-cli.zsh
@@ -0,0 +1,1 @@
+source ${0:A:h}/zfunctions/zsh-fmt-command-line/fmt-command-line.zsh
