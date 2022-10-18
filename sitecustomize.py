50fdf84361f66622f40b78dc2a7eb93e2a5f0927
Stian Lode <stian.lode@boost.ai>
2022-10-18 11:59:34 +0200

    Improves sitecustomize.py

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,27 +1,60 @@
-"""/etc/python3.9/sitecustomize.py"""
 try:
+    import traceback, os
+
     from devtools import debug
+    from devtools.ansi import sformat
+
+    def print_trace(full: bool = False):
+        """Pretty prints a compact backtrace.
+
+        Args:
+          full: If true, also print stack frames from site-packages.
+                False by default.
+        """
+        stack = traceback.extract_stack()[:-1]
+        out = []
+        for i, f in enumerate(reversed(stack)):
+            if out and "site-packages" in f[0] and not full:
+                out.append(
+                    sformat(
+                        f"\n... and {len(stack) - i} more levels",
+                        sformat.dim,
+                        sformat.italic,
+                    )
+                )
+                break
+
+            out.append(
+                sformat(f"{i}. ./{os.path.relpath(f[0])}", sformat.magenta)
+                + ":"
+                + sformat(f[1], sformat.green)
+                + " "
+                + sformat(f[2], sformat.green, sformat.italic)
+                + "\n    "
+                + sformat(f[3], sformat.dim)
+            )
+
+        print("\n".join(out) + "\n", flush=True)
+
 except ImportError:
     pass
 else:
     __builtins__["debug"] = debug
+    __builtins__["dbg"] = debug
+    __builtins__["trace"] = print_trace
+    __builtins__["bt"] = print_trace
+
 
 try:
-    import traceback, os
+    import sqlparse
 
-    def pprint_bt(userspace=True, compact=True, limit=20):
-        stack = traceback.extract_stack(limit=limit)[:-2]
-        out = []
-        for i, f in enumerate(reversed(stack)):
-            if len(out) > 0 and "site-packages" in f[0]:
-                if userspace:
-                    break
-            out.append(f"{i:2}. ./{os.path.relpath(f[0])}:{f[1]}")
-            if compact == False:
-                out.append(f'\t in {f[2]}(...) "{f[3]}"')
-        print("\n".join(out))
+    def print_sql(sql: str):
+        print(sqlparse.format(sql, reindent=True, keyword_case="upper"), flush=True)
 
 except ImportError:
-    pass
-else:
-    __builtins__["bt"] = pprint_bt
+
+    def print_sql(sql: str):
+        print(sql, flush=True)
+
+
+__builtins__["dumpsql"] = print_sql
